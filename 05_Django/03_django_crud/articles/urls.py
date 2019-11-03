648c1a8869ee1290720098767739e0cda65421c7
5dddddo <50972986+5dddddo@users.noreply.github.com>
2019-11-03 22:45:41 +0900

    Update urls.py

--- 05_Django/03_django_crud/articles/urls.py
+++ 05_Django/03_django_crud/articles/urls.py
@@ -1,9 +1,14 @@
-from django.contrib import admin
-from django.urls import path,include
-from . import views
-urlpatterns = [
-    path('<int:article_pk>/',views.detail),
-    path('', views.index),
-    path('new/', views.new),
-    path('create/', views.create),
+from django.urls import path 
+from . import views 
+  
+app_name = 'articles' 
+urlpatterns = [ 
+    path('', views.index, name='index'),   # READ Logic - Index 
+    path('new/', views.new, name='new'),    # CREATE Logic - 폼 전달 
+    path('create/', views.create, name='create'),  # CREATE Logic - DB에 저장 
+    path('<int:article_pk>/', views.detail, name='detail'),  # READ Logic - Detail 
+    path('<int:article_pk>/delete/', views.delete, name='delete'),   # DELETE Logic 
+    path('<int:article_pk>/edit/', views.edit, name='edit'),    # UPDATE Logic - 폼 전달 
+    path('<int:article_pk>/update/', views.update, name='update'),   # UPDATE Logic - DB 저장 
+    
 ]
