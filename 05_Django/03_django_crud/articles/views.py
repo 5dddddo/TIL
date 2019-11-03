9907111e4f45894bdca414083343055b16b6fd8f
5dddddo <50972986+5dddddo@users.noreply.github.com>
2019-11-03 22:48:50 +0900

    Update views.py

--- 05_Django/03_django_crud/articles/views.py
+++ 05_Django/03_django_crud/articles/views.py
@@ -24,10 +24,45 @@ def create(request):
     # render 는 views에서 index.html만 찾아서 뿌려주는 역할
     # index 함수를 실행하지 않음
     # 함수까지 실행하려면 redirect 함수 사용!
-    return redirect('/articles/')
+    # return redirect('/articles/')
+    
+    # return redirect(f'/articles/{article.pk}')       # 1 하드코딩 
+    return redirect('articles:detail', article.pk)     # 2 URL Namespace 
+   
+   
+    # .html 파일 내에서 '{% url %} 템플릿 태그' 사용했을 때 (헷갈림 주의!!!!!) 
+    # <a href="{% url 'articles:delete' article.pk %}"> 
 
+    
 # 게시글 상세정보를 가져오는 함수
 def detail(request, article_pk):
     article = Article.objects.get(pk=article_pk)
     context = {'article': article}
     return render(request, 'articles/detail.html', context)
+
+def delete(request, article_pk): 
+    article = Article.objects.get(pk=article_pk) 
+    article.delete() 
+    return redirect('articles:index') 
+
+
+# 사용자한테 게시글 수정 폼을 전달 
+def edit(request, article_pk): 
+    article = Article.objects.get(pk=article_pk) 
+    context = {'article': article} 
+    return render(request, 'articles/edit.html', context) 
+
+
+# 수정 내용 전달받아서 DB에 저장(반영) 
+def update(request, article_pk): 
+    # 1. 수정할 게시글 인스턴스 가져오기 
+    article = Article.objects.get(pk=article_pk) 
+    # 2. 폼에서 전달받은 데이터 덮어쓰기 
+    article.title = request.POST.get('title') 
+    article.content = request.POST.get('content') 
+    # 3. DB 저장 
+    article.save() 
+    # 4. 저장 끝났으면 게시글 Detail로 이동시키기 
+    # return redirect(f'/articles/{article.pk}/')   # 하드코딩 
+    return redirect('articles:detail', article.pk)  # URL Namespace 
+ 
