6c16cc08de7b4f7963f14bacea0c1dfff71b1196
5dddddo <50972986+5dddddo@users.noreply.github.com>
2019-11-03 22:49:24 +0900

    Update settings.py

--- 05_Django/03_django_crud/config/settings.py
+++ 05_Django/03_django_crud/config/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'articles',
+    'jobs',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
