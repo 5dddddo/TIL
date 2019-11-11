52acc5e5c4f23c2875d954247994288784114af3
5dddddo <50972986+5dddddo@users.noreply.github.com>
2019-11-11 21:36:28 +0900

    Update views.py

--- 05_Django/04_django_form/accounts/views.py
+++ 05_Django/04_django_form/accounts/views.py
@@ -1,11 +1,12 @@
+from IPython import embed
 from django.shortcuts import render, redirect
-
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-
-# Authentication(인증) -> 신원 확인
-#   - 자신이 누구라고 주장하는 사람의 신원을 확인하는 것
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 
 # Create your views here.
 def signup(request):
@@ -13,15 +14,16 @@ def signup(request):
         return redirect('articles:index')
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
+        # embed()
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
             return redirect('articles:index')
     else:
-        form = UserCreationForm
+        form = CustomUserCreationForm()
     context = {'form': form}
-    return render(request, 'accounts/signup.html', context)
+    return render(request, 'accounts/auth_form.html', context)
 
 def login(request):
     if request.user.is_authenticated:
@@ -29,14 +31,47 @@ def login(request):
 
     if request.method == 'POST':
         form = AuthenticationForm(request, request.POST)
+        # embed()
         if form.is_valid():
             auth_login(request, form.get_user())
-            return redirect('articles:index')
+            # embed()
+            return redirect(request.GET.get('next') or 'articles:index')
     else:
         form = AuthenticationForm()
     context = {'form': form}
-    return render(request, 'accounts/login.html', context)
+    return render(request, 'accounts/auth_form.html', context)
 
 def logout(request):
     auth_logout(request)
     return redirect('articles:index')
+
+@require_POST
+def delete(request):
+    request.user.delete()
+    return redirect('articles:index')
+
+# 회원정보 수정
+@login_required
+def update(request):
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {'form': form}
+    return render(request, 'accounts/auth_form.html', context)
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            return redirect('articles:index')
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {'form': form}
+    return render(request, 'accounts/auth_form.html', context)
