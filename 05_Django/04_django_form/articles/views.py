ea7cc2936a8129460096ca526eb8bf0dabb036c0
ooo <oea0805@gmail.com>
2019-11-12 10:01:06 +0900

    migrate 수행

--- 05_Django/04_django_form/articles/views.py
+++ 05_Django/04_django_form/articles/views.py
@@ -25,9 +25,10 @@ def create(request):
     # POST 요청일 경우 -> 게시글 생성 로직 수행
     if request.method == 'POST':
         form = ArticleForm(request.POST)                # 유효성 검증
-
         if form.is_valid():
-            article = form.save()
+            article = form.save(commit=False)
+            article.user = request.user
+            article.save()   
         return redirect('articles:detail', article.pk)
     else:
         form = ArticleForm()
@@ -79,6 +80,7 @@ def comments_create(request, article_pk):
     if request.user.is_authenticated:
         comment_form = CommentForm(request.POST)
         if comment_form.is_valid():
+            
             # save() 메서드 -> 선택 인자 : (기본값) commit=True
             # DB에 바로 저장
             # commit=False : DB에 바로 저장하는 것을 막은 후
